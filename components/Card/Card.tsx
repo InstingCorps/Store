e14b730d597419acf89866e2a2d18be16ac08b02
FahrurRozi <akungamesaya123456@gmail.com>
2023-07-06 22:15:40 +0700

    update

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -5,16 +5,15 @@ import "./styles.css"
 
 export default function ComponentCard() {
     return(
-        <div className="mt-10 grid grid-cols-3 md:grid-cols-8 gap-4 m-9">
+        <div className="mt-10 grid grid-cols-3 md:grid-cols-8 gap-4 m-3 btn-card">
         <Card className="h-auto max-w-full rounded-lg ComCard"
          imgAlt=""
          href="/mobile-legends"
          imgSrc="https://cdn1.codashop.com/S/content/mobile/images/product-tiles/ID_MLBB-M4-Codacash-tile.jpg"
          
          >
-         <Button className="btn-card" color="gray">
-          Topup
-        </Button>
+        <div className="font-extrabold content-center text-center">Mobile Legends</div>
+         <Button className="btn-card" color="gray">Topup</Button>
         </Card>
         <Card className="h-auto max-w-full rounded-lg ComCard"
          imgAlt=""
@@ -22,7 +21,8 @@ export default function ComponentCard() {
          imgSrc="https://cdn.unipin.com/images/icon_product_pages/1645066883-icon-WeChat%20Image_20220217093952.jpg"
          
          >
-            <button className="btn-card">Genshin Impact</button>
+            <div className="font-extrabold content-center text-center">Genshin Impact</div>
+            <Button color="gray">Topup</Button>
         </Card>
 
         <Card className="h-auto max-w-full rounded-lg ComCard"
