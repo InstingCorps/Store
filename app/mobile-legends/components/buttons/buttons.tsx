a71c5132745a3a92a291423245c8b48e45971086
FahrurRozi <akungamesaya123456@gmail.com>
2023-07-06 20:30:17 +0700

    update

---
+++ app/mobile-legends/components/buttons/buttons.tsx
@@ -0,0 +1,94 @@
+
+
+'use client';
+
+import { Button } from 'flowbite-react';
+
+export default function ButtonPills() {
+  return (
+    <div className="mt-10 grid grid-cols-3 md:grid-cols-8 gap-4 m-9">
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning1
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+      <Button
+        color="warning"
+        pill
+      >
+        <p>
+          Warning
+        </p>
+      </Button>
+    </div>
+  )
+}
+
+
--- app/mobile-legends/page.tsx
+++ app/mobile-legends/page.tsx
@@ -1,11 +1,12 @@
 import ComponentNavbar from "@/components/Navbar/Navbar";
+import ButtonPills from "@/app/mobile-legends/components/buttons/buttons";
 import ComponentFooter from "@/components/footer/footer";
 
 export default function MobileLegends() {
 return(
     <main>
         <ComponentNavbar />
-        
+        <ButtonPills />
         <footer>
             <ComponentFooter />
         </footer>
