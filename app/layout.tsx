6a62aee53fd21c27ae7d4946c2e86fa7391d2155
FahrurRozi <akungamesaya123456@gmail.com>
2023-07-17 17:23:47 +0700

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'RoziStore. -WebTopupTerpercaya',
   description: 'Versi Beta 0.10.1',
+  name: 'google-site-verification',
+  content : 'X0udyrnZV8QGYUrrJzz55Ow5OZI9KTUPRU_Hvss_8J8'
 }
 
 export default function RootLayout({
@@ -15,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="X0udyrnZV8QGYUrrJzz55Ow5OZI9KTUPRU_Hvss_8J8" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
