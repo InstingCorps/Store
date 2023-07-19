88a729deeb278d02bd416bbe42993729ccdb9de3
FahrurRozi <akungamesaya123456@gmail.com>
2023-07-19 17:16:43 +0700

    tes4

--- payment/BiayaPembayaran.tsx
+++ payment/BiayaPembayaran.tsx
@@ -1,22 +1,30 @@
 'use client' 
 
+import React , {useEffect} from "react"
+
 const BiayaPembayaran = (value : any) => {
 
-        let toString
-        const getPrice = sessionStorage.getItem('Price')
+  if (typeof sessionStorage !== 'undefined') {
+    let toString
+    const getPrice = sessionStorage.getItem('Price')
         
-        if (getPrice !== null) {
-            toString = parseFloat(getPrice);
-          } else {
-            toString = 0
-          }
+    if (getPrice !== null) {
+        toString = parseFloat(getPrice);
+      } else {
+        toString = 0
+      }
 
-    const Prices = toString
-    const Persen = value;
-    const TotalPersen = (Prices * Persen) / 100;
+const Prices = toString
+const Persen = value;
+const TotalPersen = (Prices * Persen) / 100;
+
+const result = Prices + TotalPersen
+const resultAsString = result.toString();
+    return resultAsString;
     
-    const result = Prices + TotalPersen
-    const resultAsString = result.toString();
-        return resultAsString;
+  } else {
+    return "error"
+  }
+       
 }
 export default BiayaPembayaran;
