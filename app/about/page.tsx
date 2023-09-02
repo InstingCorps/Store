841574d4e5b940d8fe35d913d7ba3a6a3f2381a9
FahrurRozi <akungamesaya123456@gmail.com>
2023-09-02 20:08:33 +0700

    ok

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,14 +2,14 @@
 import ComponentNavbar from "@/components/Navbar/Navbar";
 import ComponentFooter from "@/components/footer/footer";
 import { DarkThemeToggle, Flowbite } from 'flowbite-react';
-import Valid from "../mobile-legends/components/CheckoutML";
+// import Valid from "../mobile-legends/components/CheckoutML";
 
 
 export default function About() {
     return(
     <Flowbite>
       <ComponentNavbar />
-      <Valid />
+      {/* <Valid /> */}
         <footer>
       <DarkThemeToggle />
             <ComponentFooter />
