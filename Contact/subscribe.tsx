fa9e4ab2fb02af78448d9ea640fe40d9b67dba44
FahrurRozi <akungamesaya123456@gmail.com>
2023-12-10 09:06:55 +0700

    ok

--- Contact/subscribe.tsx
+++ Contact/subscribe.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { Card } from "flowbite-react"
+import { Button, Card } from "flowbite-react"
 
 export default function Subscribe() {
     return(
 
-<Card className="bg-white mt-10">
+<Card className="bg-white mt-10 m-10">
     <div className="text-slate-800 text-xl text-center font-bold font-serif">Berlangganan</div>
+    <Button>Tes</Button>
     <div className="text-2xl text-slate-800 font-extrabold text-center">Dapatkan Penawaran Terbaik Sekarang</div>
 </Card>
     )
