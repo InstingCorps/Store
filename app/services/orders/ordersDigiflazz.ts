b9e179bbbdf9223201adca6425f39efeb1ed593b
FahrurRozi <akungamesaya123456@gmail.com>
2024-01-26 21:16:15 +0700

    #2

--- app/services/orders/ordersDigiflazz.ts
+++ app/services/orders/ordersDigiflazz.ts
@@ -1,11 +1,10 @@
 
-// import Transaction from "@/app/api/database/models/transaction";
 import axios from "axios";
 
 export const OrderDigiflazz = async (encryptedData: string, password: string , transactionID:any): Promise<any> => {
 
     // const url = `/api/ordersDigiflazz/${urls}`;
-    const url = `https://rozistorebe.tokorozy.my.id/senddataordermmmsss`;
+    const url = `https://rozistorebe.tokorozy.my.id/senddataorder`;
 
     const data = {
         data: encryptedData ,
@@ -14,8 +13,6 @@ export const OrderDigiflazz = async (encryptedData: string, password: string , t
     console.log(data);
     
     const passwords = "250106"
-
-    // const trx = await Transaction.findOne({transaction_id : transactionID})
     
     if (password === passwords) {
         try {
