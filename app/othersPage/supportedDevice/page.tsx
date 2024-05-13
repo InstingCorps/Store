af15c70a062bece9f36d030c61e9e70ce94bff6a
InstingCorps <dsfirmawan@gmail.com>
2024-05-14 04:51:13 +0800

    Update page.tsx

--- app/othersPage/supportedDevice/page.tsx
+++ app/othersPage/supportedDevice/page.tsx
@@ -4,7 +4,7 @@ const supportedDevice = () => {
   return (
     <div className="text-white font-sans">
         <div className="container mx-auto p-6 max-w-2xl">
-        <h1 className="text-3xl font-bold mb-4">Selamat datang di RoziStore!</h1>
+        <h1 className="text-3xl font-bold mb-4">Selamat datang di INSTING STORE!</h1>
 
         <h6 className="mb-4">Kami ingin berbagi informasi mengenai pengalaman Anda dalam menjelajahi website kami. Saat ini, RoziStore telah dirancang khusus untuk memberikan pengalaman terbaik pada perangkat ponsel dan tablet. Namun, kami ingin memberitahukan bahwa kami masih dalam tahap pengembangan, dan sayangnya, <p className="font-bold border-b-2 inline">versi untuk PC atau laptop belum sepenuhnya didukung.</p></h6>
 
@@ -12,7 +12,7 @@ const supportedDevice = () => {
 
         <h6 className="mb-4">Meskipun demikian, untuk sementara waktu, kami ingin <p className="font-bold border-b-2 inline">merekomendasikan penggunaan perangkat ponsel atau tablet, terutama dengan sistem operasi Android, guna menikmati fitur-fitur dan layanan kami dengan optimal.</p> Kami menghargai kesabaran Anda dan berkomitmen untuk menyajikan pengalaman berbelanja yang lebih baik dan responsif di semua jenis perangkat.</h6>
 
-        <h6 className="mb-4">Terima kasih atas pengertian dan dukungan Anda selama proses pengembangan ini. Kami berjanji akan segera memberikan pembaruan mengenai ketersediaan versi untuk PC atau laptop, dan kami sangat berharap dapat memenuhi harapan Anda untuk pengalaman berbelanja yang lebih luas dan nyaman. Terima kasih telah menjadi bagian dari RoziStore!</h6>
+        <h6 className="mb-4">Terima kasih atas pengertian dan dukungan Anda selama proses pengembangan ini. Kami berjanji akan segera memberikan pembaruan mengenai ketersediaan versi untuk PC atau laptop, dan kami sangat berharap dapat memenuhi harapan Anda untuk pengalaman berbelanja yang lebih luas dan nyaman. Terima kasih telah menjadi bagian dari INSTING STORE!</h6>
     </div>
     </div>
   )
